aca1c3bc42ecefb1f7b0b3518730866943322b27
Kaushal-FarEye <kaushal.maurya@getfareye.com>
2025-02-28 00:37:19 +0530

    Initial Setup

---
+++ src/main/java/com/next_tech/com/Next_Tech_Backend/NextTechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.next_tech.com.Next_Tech_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextTechBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/next_tech/com/Next_Tech_Backend/NextTechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next_tech.com.Next_Tech_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextTechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
